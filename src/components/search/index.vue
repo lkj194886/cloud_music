52ae85d578382d76f022a44fd53444f7958e5e58
wgg <1748495608@qq.com>
2020-12-30 15:11:02 +0800

    commit

---
+++ src/components/search/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="search">
+      sb
+      <u-search :show-action="false" input-align="center"></u-search>
+  </view>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    components:{
+      
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+    .search{
+        margin-top: 50rpx;
+    }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <view class="index">
+	  <view class="serach"><search></search></view>
+	  
+	  <view class="banner"><u-swiper></u-swiper></view>
+    
+    <!-- <u-button @click="show = true">打开</u-button> -->
+  </view>
+</template>
+<script>
+
+import search from"../../components/search/index.vue"
+export default {
+  data() {
+    return {
+	  show:false
+    };
+  },
+  components:{
+	  search
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .serach{
+	  width: 85%;
+  }
+  .banner{
+	  width: 90%;
+	  margin-top: 50rpx;
+  }
+}
+
+</style>
